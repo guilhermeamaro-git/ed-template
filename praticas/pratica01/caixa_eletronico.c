4a259b2e6c6bb5d97166c13cf775475327d3c324
guilhermeamaro-git <amaroooguilherme@gmail.com>
2026-04-19 13:58:31 -0300

    corrigir erro do caixa_eletronico_c e path

--- praticas/pratica01/caixa_eletronico.c
+++ praticas/pratica01/caixa_eletronico.c
@@ -2,41 +2,37 @@
 
 int calcular_notas(int valor)
 {
+
     if (valor <= 0 || valor > 1000)
     {
         return 0;
     }
 
-    int n200 = 0;
-    int n100 = 0;
-    int n50 = 0;
-    int n20 = 0;
-    int n10 = 0;
-    int n5 = 0;
-    int n2 = 0;
+    int n200, n100, n50, n20, n10, n5, n2;
+    int resto = valor;
 
-    n200 = valor / 200;
-    valor %= 200;
+    n200 = resto / 200;
+    resto %= 200;
 
-    n100 = valor / 100;
-    valor %= 100;
+    n100 = resto / 100;
+    resto %= 100;
 
-    n50 = valor / 50;
-    valor %= 50;
+    n50 = resto / 50;
+    resto %= 50;
 
-    n20 = valor / 20;
-    valor %= 20;
+    n20 = resto / 20;
+    resto %= 20;
 
-    n10 = valor / 10;
-    valor %= 10;
+    n10 = resto / 10;
+    resto %= 10;
 
-    n5 = valor / 5;
-    valor %= 5;
+    n5 = resto / 5;
+    resto %= 5;
 
-    n2 = valor / 2;
-    valor %= 2;
+    n2 = resto / 2;
+    resto %= 2;
 
-    if (valor != 0)
+    if (resto != 0)
     {
         return 0;
     }
@@ -57,14 +53,8 @@ int main()
     notas = calcular_notas(200);
     printf("saque = 200, notas = %i => %i\n", notas, notas == 1);
 
-    notas = calcular_notas(300);
-    printf("saque = 300, notas = %i => %i\n", notas, notas == 2);
-
     notas = calcular_notas(380);
     printf("saque = 380, notas = %i => %i\n", notas, notas == 5);
-
-    notas = calcular_notas(386);
-    printf("saque = 386, notas = %i => %i\n", notas, notas == 0);
 
     return 0;
 }
