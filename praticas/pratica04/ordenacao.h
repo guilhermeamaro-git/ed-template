f6412cfaa7f2dcd98b4d64eeff8b5b50a9b25937
guilhermeamaro-git <amaroooguilherme@gmail.com>
2026-04-21 21:49:46 -0300

    feat: conclui pratica04. Fecha #7

---
+++ praticas/pratica04/ordenacao.h
@@ -0,0 +1,9 @@
+#ifndef ORDENACAO_H
+#define ORDENACAO_H
+
+// Protótipos das funções de ordenação
+void bubble_sort(int v[], int n);
+void selection_sort(int v[], int n);
+void quick_sort(int v[], int baixo, int alto);
+
+#endif
